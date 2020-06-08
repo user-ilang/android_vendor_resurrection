35dedab15f2612fde381920715fe112d12595c10
user-ilang <33061778+user-ilang@users.noreply.github.com>
2020-06-09 02:49:22 +0700

    Update Android.mk

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -12,16 +12,6 @@ include $(BUILD_PREBUILT)
 # Prebuilt apk's included in RR builds
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := MagiskManager
-LOCAL_MODULE_OWNER := rr
-LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := PRESIGNED
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := DeskClock
 LOCAL_MODULE_OWNER := rr
 LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
