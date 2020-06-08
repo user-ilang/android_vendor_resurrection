8c846939277b3406ccc884b99f6df7f2b3109043
user-ilang <33061778+user-ilang@users.noreply.github.com>
2020-06-09 03:07:28 +0700

    Delete magisk

--- config/common.mk
+++ config/common.mk
@@ -192,17 +192,6 @@ PRODUCT_PACKAGES += \
     ThemeInterfacer
 
 
-WITH_ROOT_METHOD ?= rootless
-ifeq ($(WITH_ROOT_METHOD), magisk)
-# Magisk Manager
-PRODUCT_PACKAGES += \
-    MagiskManager
-
-# Copy Magisk zip
-PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/magisk.zip:system/addon.d/magisk.zip
-endif
-
 # Exchange support
 PRODUCT_PACKAGES += \
     Exchange2
